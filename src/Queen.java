9534b3a51a43f98a58b217b16215323625271d0e
BlazZ1t <klimspy@gmail.com>
2024-02-04 04:16:05 +0300

    Added Queen movement check

--- src/Queen.java
+++ src/Queen.java
@@ -1,10 +1,184 @@
 public class Queen extends Piece {
+    Position position;
     Queen(Position position, Color color) {
         super(color);
+        this.position = position;
     }
 
     @Override
     public boolean isMovePossible(String targetCoordinate, Board board) {
-        return true;
+        int currentCoordinateLetter = position.stringValue().charAt(0);
+        int currentCoordinateNumber = position.stringValue().charAt(1);
+        int targetCoordinateLetter = targetCoordinate.charAt(0);
+        int targetCoordinateNumber = targetCoordinate.charAt(1);
+        if ((currentCoordinateLetter == targetCoordinateLetter) && Math.abs(currentCoordinateNumber - targetCoordinateNumber) >= 1) {
+            boolean flag = true;
+            if (targetCoordinateNumber > currentCoordinateNumber) {
+                for (int i = currentCoordinateNumber + 1; i < targetCoordinateNumber; i++) {
+                    String positionCheck = (char) currentCoordinateLetter + String.valueOf((char) i);
+                    if (board.getPieceViaPosition(positionCheck) != null) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag){
+                    Piece targetPiece = board.getPieceViaPosition(targetCoordinate);
+                    if (targetPiece != null){
+                        return targetPiece.getColor() != this.color;
+                    } else{
+                        return true;
+                    }
+                }
+            } else {
+                for (int i = currentCoordinateNumber - 1; i > targetCoordinateNumber; i--){
+                    String positionCheck = (char) currentCoordinateLetter + String.valueOf((char) i);
+                    if (board.getPieceViaPosition(positionCheck) != null) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag){
+                    Piece targetPiece = board.getPieceViaPosition(targetCoordinate);
+                    if (targetPiece != null){
+                        return targetPiece.getColor() != this.color;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        } else if (Math.abs(currentCoordinateLetter - targetCoordinateLetter) >= 1 && currentCoordinateNumber == targetCoordinateNumber){
+            boolean flag = true;
+            if (targetCoordinateLetter > currentCoordinateLetter){
+                for (int i = currentCoordinateLetter + 1; i < targetCoordinateLetter; i++){
+                    String positionCheck = (char) currentCoordinateLetter + String.valueOf((char) i);
+                    if (board.getPieceViaPosition(positionCheck) != null) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag){
+                    Piece targetPiece = board.getPieceViaPosition(targetCoordinate);
+                    if (targetPiece != null){
+                        return targetPiece.getColor() != this.color;
+                    } else {
+                        return true;
+                    }
+                }
+            } else {
+                for (int i = currentCoordinateLetter - 1; i > targetCoordinateLetter; i--){
+                    String positionCheck = (char) currentCoordinateLetter + String.valueOf((char) i);
+                    if (board.getPieceViaPosition(positionCheck) != null) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag){
+                    Piece targetPiece = board.getPieceViaPosition(targetCoordinate);
+                    if (targetPiece != null){
+                        return targetPiece.getColor() != this.color;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+            //Diagonal movement
+        } else if (targetCoordinateLetter > currentCoordinateLetter){
+            //Right up
+            if (targetCoordinateNumber > currentCoordinateNumber){
+                boolean flag = true;
+                int j = currentCoordinateLetter + 1;
+                for (int i = currentCoordinateNumber + 1; i < targetCoordinateNumber; i++){
+                    if (j >= targetCoordinateLetter){
+                        break;
+                    }
+                    Piece positionCheck = board.getPieceViaPosition((char) j + String.valueOf((char) i));
+                    if (positionCheck != null){
+                        flag = false;
+                        break;
+                    }
+                    j++;
+                }
+                if (flag){
+                    Piece targetPiece = board.getPieceViaPosition(targetCoordinate);
+                    if (targetPiece != null){
+                        return targetPiece.getColor() != this.color;
+                    } else {
+                        return true;
+                    }
+                }
+                //Right down
+            } else {
+                boolean flag = true;
+                int j = currentCoordinateLetter + 1;
+                for (int i = currentCoordinateNumber - 1; i > targetCoordinateNumber; i--){
+                    if (j >= targetCoordinateLetter){
+                        break;
+                    }
+                    Piece positionCheck = board.getPieceViaPosition((char) j + String.valueOf((char) i));
+                    if (positionCheck != null){
+                        flag = false;
+                        break;
+                    }
+                    j++;
+                }
+                if (flag){
+                    Piece targetPiece = board.getPieceViaPosition(targetCoordinate);
+                    if (targetPiece != null){
+                        return targetPiece.getColor() != this.color;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        } else if (targetCoordinateLetter < currentCoordinateLetter){
+            //Left up
+            if (targetCoordinateNumber > currentCoordinateNumber){
+                boolean flag = true;
+                int j = currentCoordinateLetter - 1;
+                for (int i = currentCoordinateNumber + 1; i < targetCoordinateNumber; i++){
+                    if (j <= targetCoordinateLetter){
+                        break;
+                    }
+                    Piece positionCheck = board.getPieceViaPosition((char) j + String.valueOf((char) i));
+                    if (positionCheck != null){
+                        flag = false;
+                        break;
+                    }
+                    j--;
+                }
+                if (flag){
+                    Piece targetPiece = board.getPieceViaPosition(targetCoordinate);
+                    if (targetPiece != null){
+                        return targetPiece.getColor() != this.color;
+                    } else {
+                        return true;
+                    }
+                }
+                //Left down
+            } else {
+                boolean flag = true;
+                int j = currentCoordinateLetter - 1;
+                for (int i = currentCoordinateNumber - 1; i > targetCoordinateNumber; i--){
+                    if (j <= targetCoordinateLetter){
+                        break;
+                    }
+                    Piece positionCheck = board.getPieceViaPosition((char) j + String.valueOf((char) i));
+                    if (positionCheck != null){
+                        flag = false;
+                        break;
+                    }
+                    j--;
+                }
+                if (flag){
+                    Piece targetPiece = board.getPieceViaPosition(targetCoordinate);
+                    if (targetPiece != null){
+                        return targetPiece.getColor() != this.color;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
